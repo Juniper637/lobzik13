8f0811adf36ea920c33b4ef15c2811bb31529200
Juniper637 <juniperjam@mail.ru>
2025-03-20 07:37:10 +0300

    Initial commit

---
+++ lobzikapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LobzikappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lobzikapp'
---
+++ lobzikapp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('', views.index, name='star_index'),  # Главная страница
+   path('person/<int:person_id>/', views.star_detail, name='star_detail'),  # Детальная страница
+
+   # Страница «О сайте»
+   path('about/', views.about, name='about'),
+
+   path('add/', views.add_star, name='add_star'),  # Добавление знаменитости
+
+   path('person/<slug:slug>/', views.star_detail, name='star_detail'), # Детальная страница по slug
+
+   # Знаменитости по стране
+   path('country/<slug:slug>/', views.stars_by_country, name='stars_by_country'),
+
+   # Знаменитости по виду деятельности
+   path('industry/<slug:slug>/', views.stars_by_category, name='stars_by_category'),
+]
+
+   
