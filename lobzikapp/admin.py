b8913efcd1b22ec0a033be2d779a40a0f5c55846
Juniper637 <juniperjam@mail.ru>
2025-04-01 22:47:59 +0300

    test

--- lobzikapp/admin.py
+++ lobzikapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Star, Country, Category
 
-# Register your models here.
+@admin.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date', 'country', 'is_published', 'time_create')
+    list_filter = ('country', 'categories', 'is_published')
+    search_fields = ('name', 'content')
+    fields = ('name', 'birth_date', 'country', 'categories', 'content', 'photo', 'is_published')  # Без slug
+    list_editable = ('is_published',)
+    ordering = ('-time_create',)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
